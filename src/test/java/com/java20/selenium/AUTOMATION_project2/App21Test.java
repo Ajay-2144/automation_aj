777c70b8dd9c2022f784a2da8bafd5b380ae7977
ajay <irkarajay@gmail.com>
2023-08-02 15:35:07 +0530

    final commit by aj

---
+++ src/test/java/com/java20/selenium/AUTOMATION_project2/App21Test.java
@@ -0,0 +1,28 @@
+package com.java20.selenium.AUTOMATION_project2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class App21Test 
+{
+   
+    @Test
+    public void a()
+    {
+        System.out.println("vedika");
+    }
+    public void b()
+    {
+        System.out.println("ajay");
+    }
+    public void c()
+    {
+        System.out.println("mohini");
+    }
+    public void d()
+    {
+        System.out.println("dada");
+    }
+  
+    
+}
